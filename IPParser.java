0933e08fc7b2ccabbbf5f30f5cd4cee57d141120
João Sequeira <maildosequeira@gmail.com>
2016-06-20 18:40:25 +0100

    Update and rename Parser.java to IPParser.java

--- IPParser.java
+++ IPParser.java
@@ -22,25 +22,26 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
 
-public class Parser {
+public class InterProParser {
 	
 	public static String jobID = "iprscan5-R20160424-232342-0215-12126963-pg";
 	public static String directory = "C:\\Users\\João Sequeira\\Documents\\Escola\\Projeto Bioinformática BSIstemas\\interproscan API\\";
 
-	private List<HashMap<String, Object>> xml_information;
+	List<HashMap<String, Object>> xml_information;
 	
-	public Parser() throws IOException {
+	public InterProParser(List<String> xml) throws IOException {
 		
 		super();
 		this.xml_information = new ArrayList<HashMap<String, Object>>();
-		this.xml_information = get_xml_information();
+		this.xml_information = InterProParser.xml_information(xml);
 	}
 	
-	private static HashMap<String,String> GetEc2go() throws IOException{
+	private static HashMap<String,String> GetEc2Go() throws IOException{
 	
 		URL url = new URL("http://www.geneontology.org/external2go/ec2go");
 		Scanner s = new Scanner(url.openStream());
 		HashMap<String,String>ec2go = new HashMap<String,String>();
+		while (s.next().substring(0,1).equals("!")){s.next();}
 		while (s.hasNextLine()) {
 			List<String> entry = Arrays.asList(s.nextLine().split(" "));
 			if (!(entry.get(0).equals("!"))){
@@ -51,128 +52,165 @@ public class Parser {
 		
 		return ec2go;
 	}
-
-	public static ArrayList<String> readFile(String jobID, String extension) throws IOException{
+	
+	private static HashMap<String,String> GetSl2Go() throws IOException{
 		
-		Scanner s = new Scanner(new File(directory+jobID+extension));
-		ArrayList<String> list = new ArrayList<String>();
-		while (s.hasNextLine()){
-		    list.add(s.nextLine());
+		URL url = new URL("ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/external2go/uniprotkb_sl2go");
+		Scanner s = new Scanner(url.openStream());
+		HashMap<String,String>sl2go = new HashMap<String,String>();
+		Pattern pattern = Pattern.compile("UniProtKB-SubCell:SL-[0-9]* (.+) >");
+		while (s.next().substring(0,1).equals("!")){s.next();}
+		while (s.hasNextLine()) {
+			Matcher matcher = pattern.matcher(s.nextLine());
+			if (matcher.find()){
+				List<String> entry = Arrays.asList(s.nextLine().split(" "));
+				sl2go.put(entry.get(entry.size()-1),matcher.group(1));
+			}
+		}
+		s.close();		
+		return sl2go;
+	}
+	
+	private static HashMap<String,String> GetInterPro2Go() throws IOException{
+		
+		URL url = new URL("http://www.geneontology.org/external2go/interpro2go");
+		Scanner s = new Scanner(url.openStream());
+		HashMap<String,String>interpro2go = new HashMap<String,String>();
+		Pattern pattern = Pattern.compile("InterPro:IPR[0-9]* (.+) >");
+		while (s.next().substring(0,1).equals("!")){s.next();}
+		while (s.hasNextLine()) {
+			Matcher matcher = pattern.matcher(s.nextLine());
+			if (matcher.find()){
+				List<String> entry = Arrays.asList(s.nextLine().split(" "));
+				interpro2go.put(entry.get(entry.size()-1),matcher.group(1));
+			}
 		}
 		s.close();
 		
-		return list;
+		return interpro2go;
 	}
 
-	public static String get_protein_sequence() throws IOException{
-		
-		String protein_sequence = "";
-		
-		ArrayList<String> file = readFile(jobID,".gff.txt");
-		
-		for (int i = 0; i < file.size(); i ++) {
-			
-			if (file.get(i).startsWith(">EMBOSS_001")) {
-				
-				i += 1;
-				
-				while (!(file.get(i).startsWith(">match$"))){
-					
-					protein_sequence += file.get(i);
-					i += 1;
-				}
-			}
-		}
-		return protein_sequence;	
-	}
+//	public static ArrayList<String> readFile(String jobID, String extension) throws IOException{
+//		
+//		Scanner s = new Scanner(new File(directory+jobID+extension));
+//		ArrayList<String> list = new ArrayList<String>();
+//		while (s.hasNextLine()){
+//		    list.add(s.nextLine());
+//		}
+//		s.close();
+//		
+//		return list;
+//	}
+//
+//	public static String get_protein_sequence() throws IOException{
+//		
+//		String protein_sequence = "";
+//		
+//		ArrayList<String> file = readFile(jobID,".gff.txt");
+//		
+//		for (int i = 0; i < file.size(); i ++) {
+//			
+//			if (file.get(i).startsWith(">EMBOSS_001")) {
+//				
+//				i += 1;
+//				
+//				while (!(file.get(i).startsWith(">match$"))){
+//					
+//					protein_sequence += file.get(i);
+//					i += 1;
+//				}
+//			}
+//		}
+//		return protein_sequence;	
+//	}
 	
-	private static List<HashMap<String, String>> get_gff_information() throws IOException {
-		
-		ArrayList<String> file = readFile(jobID,".gff.txt");
-		
-		List<HashMap<String, String>> domains = new ArrayList<HashMap<String, String>>();
-		
-		Pattern GOPattern = Pattern.compile("Ontology_term=(.+?);");
-		Pattern IDPattern = Pattern.compile("ID=match(.+)_[0-9]+_[0-9]+;");
-		Pattern SDPattern = Pattern.compile("signature_desc=(.+?);");
-		Pattern NamePattern = Pattern.compile("Name=(.+?);");
-		Pattern StatusPattern = Pattern.compile("status=(.+?);");
-		Pattern xrefPattern = Pattern.compile("Dbxref=(.+)");
-
-		for (int i = 0; i < file.size(); i++) {
-			
-			if (file.get(i).startsWith("EMBOSS_001\t")) {
-				
-				if (!(file.get(i).startsWith("EMBOSS_001\t."))) {
-					
-					String[] parts = file.get(i).split("\t");
-					HashMap<String,String> domain = new HashMap<String,String>();
-					
-					domain.put("Source", parts[1]);      			//database from where the result is
-					domain.put("Type", parts[2]);					//type of match
-					domain.put("Start_position", parts[3]);
-					domain.put("End_position", parts[4]);
-					domain.put("E-value", parts[5]);
-					domain.put("Strand", parts[6]);
-					domain.put("Phase", parts[7]);					//phase indicates where the feature begins with reference to the reading frame
-					
-					Matcher GOMatcher = GOPattern.matcher(parts[8]);
-					if (GOMatcher.find()){domain.put("Ontology_term", GOMatcher.group(1));}
-					
-					Matcher IDMatcher = IDPattern.matcher(parts[8]);
-					if(IDMatcher.find()){domain.put("Match_ID", IDMatcher.group(1));}
-					
-					Matcher SDMatcher = SDPattern.matcher(parts[8]);
-					if(SDMatcher.find()){domain.put("Description", SDMatcher.group(1));}
-					
-					Matcher NameMatcher = NamePattern.matcher(parts[8]);
-					if(NameMatcher.find()){domain.put("Name", NameMatcher.group(1));}
-					
-					Matcher StatusMatcher = StatusPattern.matcher(parts[8]);
-					if(StatusMatcher.find()){domain.put("Status", StatusMatcher.group(1));}
-					
-					Matcher xrefMatcher = xrefPattern.matcher(parts[8]);
-					if(xrefMatcher.find()){domain.put("Dbxref", xrefMatcher.group(1));}
-					
-					domains.add(domain);
-					
-				}
-				
-			}
-				
-			if (file.get(i).startsWith(">match")) {
-							
-				Pattern SeqPattern = Pattern.compile(">match(.+?)_[0-9]+_[0-9]+");
-				Matcher SeqMatcher = SeqPattern.matcher(file.get(i));
-				SeqMatcher.matches();
-				
-				String sequence	= "";
-				
-				i++;
-				
-				while (!(file.get(i).startsWith(">match")) && i < file.size()-1) {
-					
-					sequence += file.get(i);		
-					
-					i ++;
-								
-				}
-				
-				for (HashMap<String,String>domain:domains){
-					
-					if(domain.get("Match_ID").equals(SeqMatcher.group(1))){
-						
-						domain.put("Sequence",sequence);
-					}
-				}
-				
-			i--;
-			}
-		}
-		
-		return domains;	
-	}
-	
+//	private static List<HashMap<String, String>> get_gff_information() throws IOException {
+//		
+//		ArrayList<String> file = readFile(jobID,".gff.txt");
+//		
+//		List<HashMap<String, String>> domains = new ArrayList<HashMap<String, String>>();
+//		
+//		Pattern GOPattern = Pattern.compile("Ontology_term=(.+?);");
+//		Pattern IDPattern = Pattern.compile("ID=match(.+)_[0-9]+_[0-9]+;");
+//		Pattern SDPattern = Pattern.compile("signature_desc=(.+?);");
+//		Pattern NamePattern = Pattern.compile("Name=(.+?);");
+//		Pattern StatusPattern = Pattern.compile("status=(.+?);");
+//		Pattern xrefPattern = Pattern.compile("Dbxref=(.+)");
+//
+//		for (int i = 0; i < file.size(); i++) {
+//			
+//			if (file.get(i).startsWith("EMBOSS_001\t")) {
+//				
+//				if (!(file.get(i).startsWith("EMBOSS_001\t."))) {
+//					
+//					String[] parts = file.get(i).split("\t");
+//					HashMap<String,String> domain = new HashMap<String,String>();
+//					
+//					domain.put("Source", parts[1]);      			//database from where the result is
+//					domain.put("Type", parts[2]);					//type of match
+//					domain.put("Start_position", parts[3]);
+//					domain.put("End_position", parts[4]);
+//					domain.put("E-value", parts[5]);
+//					domain.put("Strand", parts[6]);
+//					domain.put("Phase", parts[7]);					//phase indicates where the feature begins with reference to the reading frame
+//					
+//					Matcher GOMatcher = GOPattern.matcher(parts[8]);
+//					if (GOMatcher.find()){domain.put("Ontology_term", GOMatcher.group(1));}
+//					
+//					Matcher IDMatcher = IDPattern.matcher(parts[8]);
+//					if(IDMatcher.find()){domain.put("Match_ID", IDMatcher.group(1));}
+//					
+//					Matcher SDMatcher = SDPattern.matcher(parts[8]);
+//					if(SDMatcher.find()){domain.put("Description", SDMatcher.group(1));}
+//					
+//					Matcher NameMatcher = NamePattern.matcher(parts[8]);
+//					if(NameMatcher.find()){domain.put("Name", NameMatcher.group(1));}
+//					
+//					Matcher StatusMatcher = StatusPattern.matcher(parts[8]);
+//					if(StatusMatcher.find()){domain.put("Status", StatusMatcher.group(1));}
+//					
+//					Matcher xrefMatcher = xrefPattern.matcher(parts[8]);
+//					if(xrefMatcher.find()){domain.put("Dbxref", xrefMatcher.group(1));}
+//					
+//					domains.add(domain);
+//					
+//				}
+//				
+//			}
+//				
+//			if (file.get(i).startsWith(">match")) {
+//							
+//				Pattern SeqPattern = Pattern.compile(">match(.+?)_[0-9]+_[0-9]+");
+//				Matcher SeqMatcher = SeqPattern.matcher(file.get(i));
+//				SeqMatcher.matches();
+//				
+//				String sequence	= "";
+//				
+//				i++;
+//				
+//				while (!(file.get(i).startsWith(">match")) && i < file.size()-1) {
+//					
+//					sequence += file.get(i);		
+//					
+//					i ++;
+//								
+//				}
+//				
+//				for (HashMap<String,String>domain:domains){
+//					
+//					if(domain.get("Match_ID").equals(SeqMatcher.group(1))){
+//						
+//						domain.put("Sequence",sequence);
+//					}
+//				}
+//				
+//			i--;
+//			}
+//		}
+//		
+//		return domains;	
+//	}
+//	
 //	private static String find_regex(String regex, String query) {
 //		
 //		Pattern p = Pattern.compile(regex);
@@ -182,11 +220,17 @@ public class Parser {
 //		
 //	}
 	
-	@SuppressWarnings("unchecked")
-	private static List<HashMap<String, Object>> get_xml_information() throws IOException {
+	public List<HashMap<String, Object>> get_xml_information(List<String> xml) throws IOException {
 		
-		HashMap<String,String> ec2go = GetEc2go();		
-		ArrayList<String> file = readFile(jobID,".xml.xml");
+		return InterProParser.xml_information(xml);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static List<HashMap<String, Object>> xml_information(List<String> file) throws IOException {
+		
+		HashMap<String,String> ec2go = GetEc2Go();
+		HashMap<String,String> sl2go = GetSl2Go();
+		HashMap<String,String> interpro2go = GetInterPro2Go();
 		
 		List<HashMap<String, Object>> domains = new ArrayList<HashMap<String, Object>>();
 		
@@ -222,7 +266,7 @@ public class Parser {
 				i++;
 		
 				Matcher acMatcher = acPattern.matcher(file.get(i));
-				if (acMatcher.find()){domain.put("Signature_accession_number",acMatcher.group(1));}
+				if (acMatcher.find()){domain.put("Signature_accession",acMatcher.group(1));}
 				
 				Matcher nameMatcher = namePattern.matcher(file.get(i));
 				if (nameMatcher.find()){domain.put("Name",nameMatcher.group(1));}
@@ -232,7 +276,7 @@ public class Parser {
 				if (file.get(i).trim().startsWith("<entry")) {
 					
 					Matcher entryAcMatcher = acPattern.matcher(file.get(i));
-					if (entryAcMatcher.find()) {domain.put("Entry_accession_number",entryAcMatcher.group(1));}
+					if (entryAcMatcher.find()) {domain.put("Entry_accession",entryAcMatcher.group(1));}
 					
 					Matcher descMatcher = descPattern.matcher(file.get(i));
 					if (descMatcher.find()) {domain.put("Entry_description", descMatcher.group(1));}
@@ -402,23 +446,32 @@ public class Parser {
 				
 				}
 				
-				List<String> ecs = new ArrayList<String>();
-				
 				if (domain.containsKey("Xrefs")){
 					List<List<String>> xrefs = (List<List<String>>) domain.get("Xrefs");
 					for (List<String>xref:xrefs){
 						if (ec2go.containsKey(xref.get(2))){
-							ecs.add(ec2go.get(xref.get(2)));
+							String ec = ec2go.get(xref.get(2));
+							domain.put("EC",ec);
 						}
-				domain.put("ECs",ecs);
+						if (sl2go.containsKey(xref.get(2))){
+							String name = sl2go.get(xref.get(2));
+							domain.put("Name",name);
+						}
+						if (interpro2go.containsKey(xref.get(2))){
+							String localization = interpro2go.get(xref.get(2));
+							domain.put("Subcellular localization",localization);
+						}
+				
 					}
 				}
 				
 				domain.put("Locations", allLocations);
+				
+				System.out.println(domain);
 					
 				domains.add(domain);
 			}
-		}	
+		}
 		
 		return domains;	
 	}
@@ -433,8 +486,6 @@ public class Parser {
 //			
 //			System.out.println(domain);
 //		}
-		
-		Parser parser = new Parser();
-		for (HashMap<String,Object>domain : parser.xml_information){System.out.println(domain.get("ECs"));}
+		System.out.println(GetSl2Go());
 	}
 }
